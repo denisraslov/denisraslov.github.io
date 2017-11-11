4d4d76c24599bc917288882ea11b5763710826e0
Denis Raslov <denis.raslov@yandex.ru>
2017-11-11 21:09:25 +0300

    Make a demo for react-spreadsheet-grid

---
+++ grid/app.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import GridWrapper from './components/GridWrapper';
+
+ReactDOM.render(
+    <GridWrapper />,
+    document.querySelector('#app')
+);
