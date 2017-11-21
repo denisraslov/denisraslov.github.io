23024dc177c902d8742b6850473d3b30e3329671
Denis Raslov <denis.raslov@yandex.ru>
2017-11-21 11:10:59 +0300

    Grid: Make the table with users

--- grid/components/GridWrapper.js
+++ grid/components/GridWrapper.js
@@ -1,24 +1,56 @@
 import React from 'react';
-import { Table, Input, Select } from './Grid';
+import { Grid, Input, Select } from './Grid';
+import set from 'lodash.set';
+import usersData from './../users.json';
 
-const rows = [];
-const positions = [];
+const positions = [{
+    id: 1,
+    name: 'System Architect'
+},{
+    id: 2,
+    name: 'Frontend Developer'
+}, {
+    id: 3,
+    name: 'Backend Developer'
+}, {
+    id: 4,
+    name: 'Big Data Developer'
+}, {
+    id: 5,
+    name: 'Computer Operator'
+}, {
+    id: 6,
+    name: 'Manager'
+}, {
+    id: 7,
+    name: 'Content Manager'
+}, {
+    id: 8,
+    name: 'Support manager'
+}];
 
-for (let i = 0; i < 1000; i++) {
-    rows.push({
-        id: i,
-        firstName: 'First name ' + i,
-        secondName: 'Second name ' + i,
-        positionId: 3,
-        age: i
-    });
+const contractTypes = [{
+    id: 1,
+    name: 'Full-time'
+},{
+    id: 2,
+    name: 'Part-time'
+}, {
+    id: 3,
+    name: 'Freelance'
+}];
+
+function getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-for (let i = 1; i < 6; i++) {
-    positions.push({
-        id: i,
-        name: 'Position ' + i
-    });
+for (let i = 0; i < usersData.results.length; i++) {
+    usersData.results[i].positionId = getRandomNumber(1, 8);
+    usersData.results[i].contractTypeId = getRandomNumber(1, 3);
+}
+
+function getRowKey(row) {
+    return row.login.sha1;
 }
 
 class GridWrapper extends React.PureComponent {
@@ -27,16 +59,21 @@ class GridWrapper extends React.PureComponent {
         super(props);
 
         this.state = {
-            rows
+            rows: usersData.results
         };
     }
 
-    onFieldChange(rowId, field, value) {
-        rows[rowId][field] = value;
+    onFieldChange(rowKey, field, value) {
+        const modifiedRows = [].concat(this.state.rows);
+        const row = modifiedRows.find((row) => {
+            return getRowKey(row) === rowKey;
+        });
+
+        set(row, field, value);
 
         this.setState({
-            rows: [].concat(rows),
-            blurFocus: true
+            rows: modifiedRows,
+            blurCurrentFocus: true
         });
     }
 
@@ -46,7 +83,7 @@ class GridWrapper extends React.PureComponent {
                 <div>
                     <div className="Info__Name">React Spreadsheet Grid</div>
                     <div className="Info__Description">
-                        A customizable, performant and powerful spreadsheet grid component for React
+                        Excel-like grid component for React with custom cell editors, performant scroll & resizable columns
                     </div>
                 </div>
                 <a href="https://github.com/denisraslov/react-spreadsheet-table" className="Info__Button">
@@ -54,66 +91,134 @@ class GridWrapper extends React.PureComponent {
                 </a>
             </div>,
             <div className="GridWrapper">
-                <Table
+                <Grid
                     columns={[
+                        {
+                            title: 'Photo',
+                            value: (row, {focus}) => {
+                                return (
+                                    <img
+                                        src={row.picture.thumbnail}
+                                    />
+                                );
+                            },
+                            id: 'photo',
+                            getCellClassName: () => "Grid__photoCell"
+                        },
                         {
                             title: 'First name',
                             value: (row, {focus}) => {
                                 return (
                                     <Input
-                                        value={row.firstName}
+                                        value={row.name.first}
                                         focus={focus}
-                                        onBlur={this.onFieldChange.bind(this, row.id, 'firstName')}
+                                        onBlur={this.onFieldChange.bind(this, getRowKey(row), 'name.first')}
                                     />
                                 );
                             },
-                            id: 'firstName'
+                            id: 'name.first'
                         },
                         {
-                            title: 'Second name',
+                            title: 'Last name',
                             value: (row, {focus}) => {
                                 return (
                                     <Input
-                                        value={row.secondName}
+                                        value={row.name.last}
                                         focus={focus}
-                                        onBlur={this.onFieldChange.bind(this, row.id, 'secondName')}
+                                        onBlur={this.onFieldChange.bind(this, getRowKey(row), 'name.last')}
                                     />
                                 );
                             },
-                            id: 'secondName'
+                            id: 'name.last'
+                        },
+                        {
+                            title: 'Username',
+                            value: (row, {focus}) => {
+                                return (
+                                    <Input
+                                        value={row.login.username}
+                                        focus={focus}
+                                        onBlur={this.onFieldChange.bind(this, getRowKey(row), 'login.username')}
+                                    />
+                                );
+                            },
+                            id: 'login.username'
                         },
                         {
                             title: 'Position',
                             value: (row, {focus}) => {
                                 return (
                                     <Select
+                                        items={positions}
                                         selectedId={row.positionId}
                                         isOpen={focus}
-                                        items={positions}
-                                        onChange={this.onFieldChange.bind(this, row.id, 'positionId')}
+                                        onChange={this.onFieldChange.bind(this, getRowKey(row), 'positionId')}
                                     />
                                 );
                             },
                             id: 'position'
                         },
                         {
-                            title: 'Age',
+                            title: 'Registered',
                             value: (row, {focus}) => {
                                 return (
                                     <Input
-                                        value={row.age}
+                                        value={row.registered.split(' ')[0]}
                                         focus={focus}
-                                        onBlur={this.onFieldChange.bind(this, row.id, 'age')}
+                                        onBlur={this.onFieldChange.bind(this, getRowKey(row), 'registered')}
                                     />
                                 );
                             },
-                            id: 'age'
+                            id: 'registered'
+                        },
+                        {
+                            title: 'Contract',
+                            value: (row, {focus}) => {
+                                return (
+                                    <Select
+                                        items={contractTypes}
+                                        selectedId={row.contractTypeId}
+                                        isOpen={focus}
+                                        onChange={this.onFieldChange.bind(this, getRowKey(row), 'contractTypeId')}
+                                    />
+                                );
+                            },
+                            id: 'contract'
+                        },
+                        {
+                            title: 'Location',
+                            value: (row, {focus}) => {
+                                return (
+                                    <Input
+                                        value={row.location.postcode + ', ' +
+                                            row.location.city + ', ' +
+                                            row.location.state + ', ' +
+                                            row.location.street}
+                                        focus={focus}
+                                        onBlur={this.onFieldChange.bind(this, getRowKey(row), 'location.street')}
+                                    />
+                                );
+                            },
+                            id: 'location'
                         }
                     ]}
-                    rows={rows}
-                    blurFocus={this.state.blurFocus}
-                    getRowKey={row => row.id}
-                    cellHeight={50}
+                    rows={this.state.rows}
+                    blurCurrentFocus={this.state.blurCurrentFocus}
+                    getRowKey={row => getRowKey(row)}
+                    rowHeight={60}
+                    disabledCellChecker={(row, columnId) => {
+                        return columnId === 'photo' ||
+                            columnId === 'location' ||
+                            columnId === 'registered';
+                    }}
+                    isColumnsResizable
+                    columnWidthValues={{
+                        photo: 6,
+                        position: 15,
+                        registered: 9,
+                        contract: 10,
+                        location: 25
+                    }}
                 />
             </div>
         ];
